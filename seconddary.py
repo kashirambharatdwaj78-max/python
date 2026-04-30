d440830ef2324f898affd8aee5bcfc07804a8fb0
root <root@localhost>
2026-04-30 15:25:59 +0530

    auto publish 1777542958584

--- seconddary.py
+++ seconddary.py
@@ -1,4 +1,19 @@
-def hello(a,b):
-  return a-b
-  print('hello')
-print(hello(2,3))
+def fiboni(l):
+  a=[0,1]
+  while a[len(a)-1]<l:
+    a.append(a[len(a)-1]+a[len(a)-2])
+  q=l
+  if a[len(a)-1]>l:
+    a.remove(a[len(a)-1])
+  return a
+s=fiboni(int(input("ent limiting value of series")))
+
+
+print(s)
+  
+    
+  
+
+  
+
+  
--- src/4.py
+++ src/4.py
@@ -1,1 +1,22 @@
+def r_duplicate(a):
+  d=1
+  for c in a:
+    while d<len(a):
+      if c!=a[d]:
+        d=d+1
+      else:
+        a.remove(str(c))
+    return a
+s=r_duplicate(input('enter list of canditades:').split(","))
+print(s)
+
+      
+    
+  
+    
+  
+    
+  
+
+  
 
