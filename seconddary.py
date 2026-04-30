5c7c53f2781960133058b532e8916ddd9124fff4
root <root@localhost>
2026-04-30 15:28:57 +0530

    auto publish 1777543137608

--- seconddary.py
+++ seconddary.py
@@ -1,12 +1,9 @@
 def fiboni(l):
   a=[0,1]
-  while a[len(a)-1]<l:
+  while len(a)<l:
     a.append(a[len(a)-1]+a[len(a)-2])
-  q=l
-  if a[len(a)-1]>l:
-    a.remove(a[len(a)-1])
   return a
-s=fiboni(int(input("ent limiting value of series")))
+s=fiboni(int(input("ent limiting value of series:")))
 
 
 print(s)
