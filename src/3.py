6366e288bb6a5bfdc25729d7c8ad5cdfe79cefa6
root <root@localhost>
2026-04-30 11:38:34 +0530

    auto publish 1777529314480

--- src/3.py
+++ src/3.py
@@ -1,1 +1,14 @@
+def vowels(a):
+  count=0
+  for b in a:
+    if b=="a" or b=="e" or b=="i" or b=="o" or b=="u" or b=="A" or b=="E" or b=="I" or b=="O" or b=="U":
+      count=count+1
+    else:
+      continue
+  return count
+s=vowels(str(input("enter details::")))
+print(s)
+    
+
+  
 
