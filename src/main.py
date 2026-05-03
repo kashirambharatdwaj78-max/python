17f720b9521ddf26624fce7ffd3cc53d36d1897e
root <root@localhost>
2026-05-03 15:41:57 +0530

    auto publish 1777803117084

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
+#ent an text to get its reverse
 def reverse(a):
   s=[]
   for c  in range(len(a)-1,-1,-1):
+    #these range is used to add elements to the above list
+    #in reverse
+  
     s.append(a[c])
   p=""
   i=0
   while i<len(s):
+    #these add character from reverse list in an varible
+  
     p=p+s[i]
     i+=1
   return p
