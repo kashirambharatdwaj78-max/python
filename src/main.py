524179cd7daf72538112d864e7afb60ada483adb
root <root@localhost>
2026-05-16 16:36:24 +0530

    auto publish 1778929583761

--- src/main.py
+++ src/main.py
@@ -1,27 +1,7 @@
-def CON(a):
-  p={}
-  s=[]
-  t=[]
-  for k in a:
-    i=0  
-    for d in a:
-      if k==d:
-        i+=1
-        s.append(i)
-    p[k]=i 
-  for q in p:
-    if p[q]==max(s):
-      t.append(q)   
-  print(f'''  
-MOST USED WORD'S ARE
-  
-{t}''')
-          
-CON(input("ENT A PARAGRAPH : ").split())
-
-
-
-  
-  
-
-
+def TEST(a):
+  s=list(a)
+  e=s.pop(0)
+  if len(s)!=0:
+   return TEST(s)
+  return e
+print(TEST(input("ent ")))  
