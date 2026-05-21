2bd75375b771743db299bdbf3f0ace4cadcec068
root <root@localhost>
2026-05-21 09:28:31 +0530

    auto publish 1779335911387

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-o=input("ent ").split(" ")
-print(o)
+def MIN(a):
+  max=a[0]
+  for k in a:
+    if max<k:
+      max=k
+  return max
+def S(a):
+  a.remove(MIN(a))
+  return MIN(a)
+print(S(list(map(int,input("ENT ARRAY :").split(",")))))
+
+  
+ 
--- test.py
+++
@@ -1,7 +0,0 @@
-def CON(a):
-  if a==1:
-    return "1"
-  return CON(a//2)+str(a%2)
-print(CON(int(input("ENT NO."))))
-  
-  
